c4f288a93dc74a5e4b573f689af34deaf55b34a0
Daniel da Silva <dsilvaba@pm.me>
2026-02-07 12:58:36 +0100

    Test

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "displaylib_16/displaylib_16_Font.hpp"
 #include "displaylib_16/st7789.hpp"
 #include "hardware/spi.h"
 #include "pico/stdio.h"
@@ -16,13 +17,18 @@ int main() {
 
         tft.setupGPIO(Pins::TFT_RST, Pins::TFT_DC, Pins::TFT_CS, Pins::TFT_SCLK,
                       Pins::TFT_MOSI);
-        tft.TFTInitScreenSize(0, 0, 240, 240);
-        tft.TFTInitSPIType(32000, spi0);
+
+        tft.TFTInitScreenSize(52, 40, 135, 240);
+        tft.TFTInitSPIType(24000, spi0);
         tft.TFTST7789Initialize();
+        tft.setRotation(ST7789_TFT::Degrees_90);
 
         tft.fillScreen(ST7789_TFT::C_BLACK);
         tft.setTextColor(ST7789_TFT::C_WHITE);
-        tft.writeCharString(20, 100, (char *)"Hello, world!");
+
+        tft.setFont(font_default);
+
+        tft.writeCharString(10, 50, (char *)"Hello, world!");
 
         while (true) {
                 tight_loop_contents();
